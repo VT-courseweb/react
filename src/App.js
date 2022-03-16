cde006c5e54429a495cc71dfb105157f709c9a2c
jinsw1081 <43848395+jinsw1081@users.noreply.github.com>
2022-03-16 11:53:01 +0900

    Initialize React Project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// powershell에 작성 npm install bootstrap후
+import 'bootstrap/dist/css/bootstrap.min.css'; // 리액트에 부트스트랩연결시 
 import React from 'react';
+import { Route, Routes} from 'react-router-dom';  //라우터
+import { Main, Login, Join, Info } from './pages';
+import Header from './pages/Header';  //페이지 헤더만든부분
+import {Button} from "react-bootstrap"; //부트스트랩중 버튼  연결
 
-console.log(React.version);     //리엑트버전확인
+
+
+//console.log(React.version);     //리엑트버전확인
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-        <h3>hello,world</h3>
+        <Header></Header>
+        <Routes>
+            <Route exact ={true} path="/" element={<Main/>}/>
+            <Route path="/join" element={<Join/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/info" element={<Info/>} />
+        </Routes>
       </header>
+      <Button>Boot strap</Button>
     </div>
   );
 }
